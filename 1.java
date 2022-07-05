82699230bdad53021264d20dace2dfd6bbaa987a
kamisetty0506 <106682545+kamisetty0506@users.noreply.github.com>
2022-07-05 20:23:01 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,6 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sai Manoj"); 
-        System.out.println("Hello, Kamisetty!"); 
+        System.out.println("Hello, Kamisetty Sai Manoj"); 
     }
 }
