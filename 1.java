2f973847887d18434c897b5ea4d35a04aa4e250b
kamisetty0506 <106682545+kamisetty0506@users.noreply.github.com>
2022-07-05 18:41:12 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,5 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, Sai!"); 
+        System.out.println("Hello, Manoj!");
+        System.out.println("Hello, Kamisetty!");
+        System.out.println("Hello, 8686860667!");
+        System.out.println("Hello, kamisettysaimanoj@gmail.com!");
+        System.out.println("Hello, Cumbum!");
+        System.out.println("Hello, Andhra Pradesh!");
     }
 }
